c0a43b98f10be5132eeb8301e04743a97add2ecd
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2023-06-06 12:29:48 +0100

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,19 +4,22 @@
 #include <stdlib.h>
 
 /**
-* Description: signly linked list node structure For alx Technical interview preparation 
-* struct listint_s - singly linked list
-* @ n: integer
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint *add_nodeint(listint_t **head, const int n)
-  void free_listint(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
