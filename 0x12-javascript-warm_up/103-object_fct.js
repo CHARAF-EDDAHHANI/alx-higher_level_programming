515d3a32fa5f6057a4b597632ebbeccdd3cf73f3
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2023-11-14 20:16:38 +0100

    FIX

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,15 @@
-103-object_fct.js
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
